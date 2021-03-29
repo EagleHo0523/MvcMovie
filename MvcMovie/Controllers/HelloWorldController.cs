d3cf48b3da7af68df59aa0eccef06f78cc156f36
EagleHo <eagleho@iscom.com.tw>
2021-03-29 10:20:43 +0800

    init

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /**public string Index()
+        {
+            return "This is my default action...";
+        }**/
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+
+        public IActionResult Welcome(string name, int numTimes = 1) // https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View(); // Welcome.cshtml
+        }
+        /** public string Welcome(string name, int ID = 1)  // https://localhost:{PORT}/HelloWorld/Welcome/3?name=Rick
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }**/
+        /**public string Welcome(string name, int numTimes = 1)    // https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }**/
+        /**public string Welcome()  // https://localhost:{PORT}/HelloWorld/Welcome
+        {
+            return "This is the Welcome action method...";
+        }**/
+    }
+}
